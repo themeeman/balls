3633dfbd94e42d84bfd0a20445f0d620bd66e7b9
tomvanwoow <tom@vanhowe.com>
2017-05-05 20:25:10 +0100

    Update js.js

--- js.js
+++ js.js
@@ -5,10 +5,11 @@ function random(min, max) {
 function spawnball() {
   var ball = document.createElement("div");
   ball.id = "ball" + ballid;
-  ballid += 1
-  ball.class = "ball"
+  ballid += 1;
+  ball.class = "ball";
   ball.style.top = random(1, 100) + "px";
   ball.style.left = random(1, 100) + "px";
   ball.style.cssText += "background-color: '#FF0000';";
+  document.body.appendChild(ball);
   alert(ball.style.cssText);
 }
