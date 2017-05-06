662e15da32275420c1420511f21b527ce73214e2
tomvanwoow <tom@vanhowe.com>
2017-05-06 09:26:22 +0100

    Update js.js

--- js.js
+++ js.js
@@ -8,6 +8,7 @@ function spawnball() {
   ballid += 1;
   ball.className = "ball";
   ball.style.top = random(1, window.innerHeight-10) + "px";
-  ball.style.left = random(1, window.innerWidth-10) + "px";
+  ball.style.left = random(1, window.innerWidth-10) + "px"
+  ball.style.backgroundColor = "rgb(" + random(0,255) + "," + random(0,255) + "," + random(0,255) + ")";
   document.body.appendChild(ball);
 }
