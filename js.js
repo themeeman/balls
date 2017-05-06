1667bd5ea32019ce59b4c420565204c7ff0065e8
tomvanwoow <tom@vanhowe.com>
2017-05-06 09:36:00 +0100

    Update js.js

--- js.js
+++ js.js
@@ -3,13 +3,13 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 function spawnball() {
-  var size = random(10, 100)
+  var size = random(10, 100);
   var ball = document.createElement("div");
   ball.id = "ball" + ballid;
   ballid += 1;
   ball.className = "ball";
   ball.style.top = random(1, window.innerHeight-size) + "px";
-  ball.style.left = random(1, window.innerWidth-size) + "px"
+  ball.style.left = random(1, window.innerWidth-size) + "px";
   ball.style.backgroundColor = "rgb(" + random(0,255) + "," + random(0,255) + "," + random(0,255) + ")";
   ball.style.height = size + "px";
   ball.style.width = size + "px";
